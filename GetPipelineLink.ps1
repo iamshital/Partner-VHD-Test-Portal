59a2bb1d86a84a68d71cd69886f030de61b0cdc8
Shital Savekar <iamshital@outlook.com>
2018-01-07 17:48:01 -0800

    Added GetPipelineLink.ps1

---
+++ GetPipelineLink.ps1
@@ -0,0 +1,14 @@
+param
+(
+    [string]$pipelineName
+)
+Import-Module .\vendortesting.psm1 -Force -Global
+
+$buildURLSplitted = $env:BUILD_URL.Replace('https://','').Replace('http://','').Split('/')
+$ciURL = $buildURLSplitted[0]
+$pipelineID = ([int](Get-Content -Path "J:\Jenkins\jobs\$pipelineName\nextBuildNumber"))
+$finalURL = "http://" + "$ciURL" + "/blue/organizations/jenkins" + "/$pipelineName" + "/detail" + "/$pipelineName" + "/$pipelineID" + "/pipeline"
+LogText -text "------------------------Test Pipeline Scheduled-----------------------"
+LogText -text "Your pipeline should be running at below location.."
+LogText -text "$finalURL"
+LogText -text "----------------------------------------------------------------------"
