7c2fd1325c8481fc89f0d4adb14fa4471b1f6031
Shital Savekar <iamshital@outlook.com>
2018-03-04 22:26:57 -0800

    Changed Jenkins installation drive

--- GetPipelineLink.ps1
+++ GetPipelineLink.ps1
@@ -6,7 +6,7 @@ Import-Module .\vendortesting.psm1 -Force -Global
 
 $buildURLSplitted = $env:BUILD_URL.Replace('https://','').Replace('http://','').Split('/')
 $ciURL = $buildURLSplitted[0]
-$pipelineID = ([int](Get-Content -Path "J:\Jenkins\jobs\$pipelineName\nextBuildNumber"))
+$pipelineID = ([int](Get-Content -Path "C:\Jenkins\jobs\$pipelineName\nextBuildNumber"))
 $finalURL = "http://" + "$ciURL" + "/blue/organizations/jenkins" + "/$pipelineName" + "/detail" + "/$pipelineName" + "/$pipelineID" + "/pipeline"
 LogText -text "------------------------Test Pipeline Scheduled-----------------------"
 LogText -text "Your pipeline should be running at below location.."
